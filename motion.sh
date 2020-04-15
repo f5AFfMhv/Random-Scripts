814e16a186ec2b6b80f3442c65ccb515f399faaa
f5AFfMhv <f5AFfMhv@protonmail.com>
2020-04-15 12:29:51 +0300

    updated

--- motion.sh
+++ motion.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Executed by the motion daemon. Saves video file with motion and sends video via signal
+
+TEL_NR="XXXXXXXXXX"
 
 echo "motion detected $(date); video file - $1" >> /var/lib/motion/motion.txt
 ffmpeg -i $1 -acodec libfaac -b:a 128k -vcodec mpeg4 -b:v 1200k -flags +aic+mv4 /var/lib/motion/output.mp4
-signal-cli --dbus-system send -m "motion detected $(date)" 37062960470 -a /var/lib/motion/output.mp4
+signal-cli --dbus-system send -m "motion detected $(date)" $TEL_NR -a /var/lib/motion/output.mp4
 rm -f $1
 rm -f /var/lib/motion/output.mp4
