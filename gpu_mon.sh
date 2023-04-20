2ad0f241f1aa12bb8580dbb0652d941a718103af
mj <mj@home.lab>
2023-04-21 00:26:43 +0300

    update comments

--- gpu_mon.sh
+++ gpu_mon.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
+# Logs GPU and CPU temperatures to file
 
-#Tikrina ir raso i loga GPU ir CPU temp ir GPU power
-
-
-FAILAS=$(date +%Y%m%d%H%M)
+FILE_NAME=$(date +%Y%m%d%H%M)
 while true
 do
-	echo -e "$(date +%H:%M:%S)\tCPU: $(sensors | grep Tctl | awk '{print $2}')\tGPU: $(nvidia-smi -q -d  Power | grep Draw | awk '{print $4}')W\t +$(nvidia-smi -q -d TEMPERATURE | grep "GPU Current Temp" | awk '{print $5}')°C" >> /var/log/gpumoni/gpumoni-$FAILAS.log
+	echo -e "$(date +%H:%M:%S)\tCPU: $(sensors | grep Tctl | awk '{print $2}')\tGPU: $(nvidia-smi -q -d  Power | grep Draw | awk '{print $4}')W\t +$(nvidia-smi -q -d TEMPERATURE | grep "GPU Current Temp" | awk '{print $5}')°C" >> /var/log/gpumoni/gpumoni-$FILE_NAME.log
 	sleep 1
 done
