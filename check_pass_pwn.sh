2fcc6f8b15460321eb44262ee5794900ef6106bf
mj <mj@home.lab>
2023-04-21 00:24:25 +0300

    update script

--- check_pass_pwn.sh
+++ check_pass_pwn.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-#########################################################
-#Redaguota: 19-03-12									#
-#Paskirtis: Paskaiciuoja ivesto slaptazodzio sha1 hasha	#
-#			ir patikrina pwnedpasswords duomenu bazeje	#
-#			pagal pirmus 5 hash simbolius				#
-#########################################################
+# Martynas J.
+# Takes password as an input and calculates sha1 sum.
+# Gets all sha1 sums from pwnedpasswords database, that matches 5 first chars with calculated sum.
+# Finds if there are exact sha1 sum matches localy and returns results.
 
 clear
-# Gauti slaptazodi
-echo "Ivesk slaptazodi"
-read PASSWORD
+echo "Enter password"
+read -s PASSWORD
 
+clear
 HASH=$(echo -n $PASSWORD | sha1sum | awk '{print $1}')
 HASH_PREFIX=$(echo $HASH | cut -c -5)
 HASH_SUFIX=$(echo $HASH | cut -c6-)
-curl https://api.pwnedpasswords.com/range/$HASH_PREFIX | grep -i $HASH_SUFIX > /tmp/pwn_result
-
-clear
-echo "--------------------------------------------------------------"
-echo -n "SHA1 hash: "
+echo -n "SHA1 hash of your password: "
 echo $HASH
-echo -n "Tikrinu DB pagal: "
+echo -n "Getting all records from DB that matches: "
 echo $HASH_PREFIX
-echo -n "Isrenku is rezultatu: "
-echo $HASH_SUFIX
-echo "--------------------------------------------------------------"
-echo "Rezultatas:"
-cat /tmp/pwn_result
-rm -f /tmp/pwn_result
+PWN_COUNT=$(curl -s https://api.pwnedpasswords.com/range/$HASH_PREFIX | grep -i $HASH_SUFIX | awk -F ':' '{print $2}')
+echo "Number of times your password was leaked:" ${PWN_COUNT}
