e396b757dc50e4399c9e8a8fb01c9672c3ea08ef
mj <mj@home.lab>
2023-04-21 00:34:31 +0300

    update comments

--- sort_files.sh
+++ sort_files.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 
-#################################################
-# Redaguota:	2018-07-18						#
-# Paskirtis:	Isrusiuoja failus kataloge		#
-#				pagal pirmas 2 pavadinimo		#
-#				raides ir sukuria jiem aplanka	#
-#				direktorijoje					#
-#################################################
-
+# Sorts files in directory by first 2 name letters
+# and moves them to created directories
 
 clear
-# Gauti direktorija
-echo "Pilnas kelias i direktorija"
+echo "Full path to directory"
 read DIRECTORY
 cd $DIRECTORY
 
 clear
 FILES=$(ls $DIRECTORY)
 COUNT="1"
-ALL_FILES=( $FILES ) # konvertuoti i array
+ALL_FILES=( $FILES )
 
 for FILE_NAME in $FILES
 do
 	dir_name=$(echo $FILE_NAME | cut -c1-2)
 	mkdir $dir_name > /dev/null 2>&1
 	mv ./$FILE_NAME ./$dir_name
-	
-	echo "$FILE_NAME 	perkeltas i 	$dir_name 	($COUNT/${#ALL_FILES[@]})"
+
+	echo "$FILE_NAME 	moved to	$dir_name 	($COUNT/${#ALL_FILES[@]})"
 	COUNT=$((COUNT+1))
 done
 
 echo " "
 echo "------------------------"
-echo "-        BAIGTA!       -"
+echo "-         DONE!        -"
 echo "------------------------"
